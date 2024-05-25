bf2f824942639e6f33862b77d7d39a23b02a98be
zaeemahmed <zahmed.bscs18seecs@seecs.edu.pk>
2024-05-25 15:48:12 +0500

    chore: setup frontend

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: 'conversations',
+    element: <div>Conversations</div>,
+    children: [
+      {
+        path: ':id',
+        element: <div>hello children</div>
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
